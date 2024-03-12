227240f67c775432f73350366885ebab1628e266
Krzysztof K <krys.mcrft@gmail.com>
2024-03-12 14:49:16 +0100

    Add avg method

--- Apbd1/Apbd1/Program.cs
+++ Apbd1/Apbd1/Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, User!!");;
+Console.WriteLine("Hello, User!!");
+Console.WriteLine(GetAvg([1,2,3,4,5]));
+
+static double GetAvg(int[] arr)
+{
+    int sum = 0;
+    foreach (var num in arr)
+    {
+        sum += num;
+    }
+    return (double)sum / arr.Length;
+}
