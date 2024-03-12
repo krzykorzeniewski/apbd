dfdd1c0a822c679cbc9198b4c2be93e53f404167
Krzysztof K <korzeniewski@duck.com>
2024-03-12 18:27:11 +0100

    zmieniono zmienna w petli GetAvg

--- Apbd1/Apbd1/Program.cs
+++ Apbd1/Apbd1/Program.cs
@@ -6,9 +6,9 @@ Console.WriteLine(GetMax([1,2,3,4,5]));
 static double GetAvg(int[] arr)
 {
     int sum = 0;
-    foreach (var num in arr)
+    foreach (var number in arr)
     {
-        sum += num;
+        sum += number;
     }
     return (double)sum / arr.Length;
 }
