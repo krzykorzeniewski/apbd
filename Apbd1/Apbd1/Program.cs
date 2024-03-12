6b70e2e71e3d71a30570a3e12c4411dba0c4e9a3
Krzysztof K <korzeniewski@duck.com>
2024-03-12 18:17:10 +0100

    Dodano metode GetMax

--- Apbd1/Apbd1/Program.cs
+++ Apbd1/Apbd1/Program.cs
@@ -1,7 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-
+﻿
 Console.WriteLine("Hello, User!!");
 Console.WriteLine(GetAvg([1,2,3,4,5]));
+Console.WriteLine(GetMax([1,2,3,4,5]));
 
 static double GetAvg(int[] arr)
 {
@@ -11,4 +11,17 @@ static double GetAvg(int[] arr)
         sum += num;
     }
     return (double)sum / arr.Length;
+}
+
+static int GetMax(int[] arr)
+{
+    int max = 0;
+    foreach (var num in arr)
+    {
+        if (num > max)
+        {
+            max = num;
+        }
+    }
+    return max;
 }
