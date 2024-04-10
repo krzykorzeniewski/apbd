d28e18fb319cf9885ee14920dd18e896804302f1
Krzysztof K <korzeniewski@duck.com>
2024-04-10 19:43:09 +0200

    dodano model wizyty

---
+++ Apbd4/Apbd4/models/Visit.cs
@@ -0,0 +1,9 @@
+﻿namespace Apbd4.models;
+
+public class Visit
+{
+    public DateTime Date { get; set; }
+    public Animal Animal { get; set; }
+    public string Description { get; set; }
+    public double Cost { get; set; }
+}
