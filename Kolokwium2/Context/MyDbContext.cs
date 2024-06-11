6ec1e277a76df7fbf7dc91f29769e2ed19eee741
Krzysztof K <krys.mcrft@gmail.com>
2024-06-11 13:51:14 +0200

    init

---
+++ Kolokwium2/Context/MyDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Kolokwium2.Context;
+
+public class MyDbContext : DbContext
+{
+    protected MyDbContext()
+    {
+    }
+
+    public MyDbContext(DbContextOptions options) : base(options)
+    {
+    }
+}
---
+++ Kolokwium2/Program.cs
@@ -0,0 +1,24 @@
+using Kolokwium2.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<MyDbContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+});
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+app.UseHttpsRedirection();
+app.Run();
